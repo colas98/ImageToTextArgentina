239b9963d14f00e62ac7033f993b1ab99bb8ae66
colas98 <colas98@gmail.com>
2023-04-24 11:20:06 -0300

    main and .gitignore

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ import pytesseract
 def pdf_to_img(pdf_file):
     return pdf2image.convert_from_path(pdf_file, first_page= 20, last_page=21)
 
-
-def  ocr_core(file):
+def ocr_core(file):
     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(file, lang='spa')
     return text
@@ -27,6 +26,8 @@ def print_pages(pdf_file):
     for pg, img in enumerate(images):
         img = preprocess_image(img)
         print(ocr_core(img))
+        text = ocr_core(img)
+        print('next')
 
-book_path = 'C:/Users/Hp/OneDrive/Escritorio/libro_sabina.pdf'
+book_path = 'C:/Users/Hp/OneDrive/Escritorio/libro_3.pdf'
 print_pages(book_path)
