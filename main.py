bfc59922816529d9d49ede2c5b70388a4d18d710
colas98 <colas98@gmail.com>
2023-05-05 20:23:32 -0300

    automatization and standardization for pyqt5 structure

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # Tesseract
 
+import cv2
+import pytesseract
+import fastwer
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import Image, display
+from preprocessing_image import preprocess_image
+
+
+# TESSERACT MAIN
+
+# Tesseract
+
 # Convert document from pdf to image https://stackoverflow.com/questions/29657237/tesseract-ocr-pdf-as-input
 import cv2
 import pdf2image
 import numpy as np
-from preprocessing_image import preprocess_image
 try:
     from PIL import Image
 except ImportError:
@@ -12,22 +26,81 @@ except ImportError:
 import pytesseract
 
 
-def pdf_to_img(pdf_file):
-    return pdf2image.convert_from_path(pdf_file, first_page= 20, last_page=21)
+
+# parser = argparse.ArgumentParser(description="This program extracts provision numbers from a set of documents.")
+# parser.add_argument("-i", "--input_dir", help="Input directory for the files to be modified")
+# parser.add_argument("-o", "--output_dir", help="Output directory for the files to be modified")
+# args = parser.parse_args()
+#
+# input_dir = args.input_dir
+# output_dir = args.output_dir
+#
+# if os.path.exists(output_dir):
+#     shutil.rmtree(output_dir)
+# os.makedirs(output_dir)
+#
+# im_names = glob.glob(os.path.join(input_dir, '*.png')) + \
+#            glob.glob(os.path.join(input_dir, '*.jpg')) + \
+#            glob.glob(os.path.join(input_dir, '*.jpeg'))
+
+
+def pdf_to_img(pdf_file, FIRST_PAGE, LAST_PAGE):
+    return pdf2image.convert_from_path(pdf_file, first_page= FIRST_PAGE, last_page=LAST_PAGE)
 
 def ocr_core(file):
     pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(file, lang='spa')
     return text
 
+# Show the sample images that we will work on (Need to create a folder to upload sample images in Colab environment)
+txt_testing = os.listdir('C:/Users/Hp/PycharmProjects/ImageToTextArgentina/data')
+# txt_testing = sorted(txt_testing)
+# txt_testing = [doc for doc in txt_testing if '.txt' in doc]
+# sample = txt_testing[0]
 
-def print_pages(pdf_file):
-    images = pdf_to_img(pdf_file)
+pdf_list = os.listdir('C:/Users/Hp/PycharmProjects/ImageToTextArgentina/data')
+# pdf_list = sorted(pdf_list)
+# pdf_list = [doc for doc in pdf_list if '.pdf' in doc]
+# pdf_list = pdf_list[0]
+
+pdf_dict = {
+'libro_1.pdf': {'first_page': 20, 'last_page': 21, 'sample': 'libro_1_sample.txt', 'sampling': True, 'sampling_type': 1,
+                'cropping_image': False, 'thresholding_image': False, 'resizing_image': False},
+} # pedir first_page y last_page como argumento
+
+# Create empty dataframe to store output
+df_output = pd.DataFrame(columns = ['pdf_filename', 'sample_filename', 'txt_reference', 'ocr_output', 'cer', 'wer'])
+
+# df_output['pdf_filename'] = pdf_list
+df_output['pdf_filename'] = 'libro_1.pdf'
+
+for num, pdf in enumerate(pdf_list):
+    first_page = pdf_dict[pdf]['first_page']
+    last_page = pdf_dict[pdf]['last_page']
+    images = pdf_to_img('C:/Users/Hp/PycharmProjects/ImageToTextArgentina/data/' + pdf, first_page, last_page)
+    with open('C:/Users/Hp/PycharmProjects/ImageToTextArgentina/data/' + pdf_dict[pdf]['sample'],  'r', encoding='utf-8') as f :
+        reference_text = f.read()
+    output_text = ''
+    pnr_area = [150, 450, 1600, 1150]
+    dict_preprocessing = {}
     for pg, img in enumerate(images):
-        img = preprocess_image(img)
-        print(ocr_core(img))
-        text = ocr_core(img)
-        print('next')
+        img = preprocess_image(img, dict_preprocessing = pdf_dict[pdf])
+        output_aux_text = ocr_core(img)
+        output_text += output_aux_text
+        #print(pdf)
+        #print(img)
+        #print(output_aux_text)
+    output_text = output_text.replace("\n", "")
+    output_text = output_text.replace("-\n", "")
+    reference_text = reference_text.replace("\n", "")
+    reference_text = reference_text.replace("-\n", "")
+    cer = fastwer.score_sent(output_text, reference_text, char_level=True)
+    wer = fastwer.score_sent(output_text, reference_text, char_level=False)
+    df_output.loc[df_output['pdf_filename'] == pdf, 'sample_filename'] = pdf_dict[pdf]['sample']
+    df_output.loc[df_output['pdf_filename'] == pdf, 'txt_reference'] = reference_text[0:200]
+    df_output.loc[df_output['pdf_filename'] == pdf, 'ocr_output'] = output_text[0:200]
+    df_output.loc[df_output['pdf_filename'] == pdf, 'cer'] = cer
+    df_output.loc[df_output['pdf_filename'] == pdf, 'wer'] = wer
 
-book_path = 'C:/Users/Hp/OneDrive/Escritorio/libro_3.pdf'
-print_pages(book_path)
+df_output
+
--- preprocessing_image.py
+++ preprocessing_image.py
@@ -1,19 +1,83 @@
+# PREPROCESSING IMAGES 
+
 import numpy as np
 import cv2
 
-def preprocess_image(img):
-    # Rescale the image, if needed.
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+from IPython.display import Image, display
+
+
+def resizing_image(img, argument):
+    '''
+    1: Shrink
+    2: Enlarge Cubic
+    3: Enlager Linear
+    '''
+    switcher = {
+        1: cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA),
+        2: cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC),
+        3: cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+    }
+    return switcher.get(argument, "Invalid method")
+
+
+def threshold_image(img, argument):
+    '''
+    # 1. Binary-Otsu w/ Gaussian Blur (kernel size = 9)                               #
+    # 2. Binary-Otsu w/ Gaussian Blur (kernel size = 7)                               #
+    # 3. Binary-Otsu w/ Gaussian Blur (kernel size = 5)                               #
+    # 4. Binary-Otsu w/ Median Blur (kernel size = 5)                                 #
+    # 5. Binary-Otsu w/ Median Blur (kernel size = 3)                                 #
+    # 6. Adaptive Gaussian Threshold (31,2) w/ Gaussian Blur (kernel size = 5)        #
+    # 7. Adaptive Gaussian Threshold (31,2) w/ Median Blur (kernel size = 5)
+    '''
+
+    switcher = {
+        1: cv2.threshold(cv2.GaussianBlur(img, (9, 9), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        2: cv2.threshold(cv2.GaussianBlur(img, (7, 7), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        3: cv2.threshold(cv2.GaussianBlur(img, (5, 5), 0), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        4: cv2.threshold(cv2.medianBlur(img, 5), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        5: cv2.threshold(cv2.medianBlur(img, 3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1],
+        6: cv2.adaptiveThreshold(cv2.GaussianBlur(img, (5, 5), 0), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, 31, 2),
+        7: cv2.adaptiveThreshold(cv2.medianBlur(img, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2),
+    }
+    return switcher.get(argument, "Invalid method")
+
+
+def crop_image(img, start_x, start_y, end_x, end_y):
+    cropped = img[start_y:end_y, start_x:end_x]
+    return cropped
+
+
+def preprocess_image(img, dict_preprocessing):
     img = np.array(img)
-    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-    # Convert to gray
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Apply dilation and erosion to remove some noise
     kernel = np.ones((1, 1), np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
     img = cv2.erode(img, kernel, iterations=1)
-    # Apply blur to smooth out the edges
-    img = cv2.GaussianBlur(img, (5, 5), 0)
-    # Apply threshold to get image with only b&w (binarization)
-    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    if dict_preprocessing['cropping_image'] is True:
+        # Crop the areas where provision number is more likely present
+        img = crop_image(img, dict_preprocessing['pnr_area'][0], dict_preprocessing['pnr_area'][1],
+                         dict_preprocessing['pnr_area'][2], dict_preprocessing['pnr_area'][3])
+        img_show = Image.fromarray(img, 'RGB')
+        display(img_show)
+
+    # Rescale the image, if needed.
+    if dict_preprocessing['resizing_image'] is True:
+        img = resizing_image(img, dict_preprocessing['resizing_method'])
+        img_show = Image.fromarray(img, 'RGB')
+        display(img_show)
+
+    # Apply blurring and thresholding
+    if dict_preprocessing['thresholding_image']  is True:
+        img = threshold_image(img, dict_preprocessing['thresholding_method'])
+        img_show = Image.fromarray(img, 'RGB')
+        display(img_show)
+
     return img
